447acf99b9fe6a5a9b1db92dd8db5e35dba3bdbf
akilaisuru <akilaisurukahingala@gmail.com>
2026-08-25 14:45:18 +0530

    first commit

---
+++ src/test/java/com/ticketapp/booking/TicketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketapp.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
